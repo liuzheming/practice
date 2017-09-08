51cf1d726a28631565f65ecb4c9acb5867be5a82
liuzheming <liu_0006@126.com>
2017-09-08 15:16:45 +0800

    //数组测试

--- src/main/java/dataStructure/DataStructureTest.java
+++ src/main/java/dataStructure/DataStructureTest.java
@@ -1,5 +1,7 @@
 package dataStructure;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -9,21 +11,39 @@ import org.junit.Test;
  */
 public class DataStructureTest {
 
+    private int length = 100;
+
+    private MyOrderArray array = new MyOrderArray(length);
+
+    @Before
+    public void fillArray() {
+        for (int i = length - 1; i >= 0; i--) {
+            array.insert(i);
+        }
+    }
+
 
     @Test
     public void testMyArray() {
 
-        MyArray array = new MyArray(10);
-        array.insert(1);
-        array.insert(2);
-        array.insert(3);
-        array.insert(4);
-        array.insert(5);
-        array.delete(3);
-        array.update(1,110);
-        System.out.println(array.get(2));
-        array.display();
 
+        // 测试是否正确排序
+        for (int i = 0; i < array.size(); i++) {
+            Assert.assertEquals(array.get(i), Long.valueOf(i));
+        }
+
+        // 测试删除
+        array.delete(8);
+//        Assert.assertEquals(array.size(), length - 1);
+
+        for (int i = 0; i < 20; i++) {
+            Long n = (long) (Math.random() * 100);
+            System.out.println(n);
+            Assert.assertEquals(array.get(array.search(n)), n);
+        }
+
+        // 打印
+        array.display();
     }
 
 }
