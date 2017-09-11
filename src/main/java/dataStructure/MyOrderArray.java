0550406ed4ab008fc21b45894fb94ebdfd0229f6
liuzheming <liu_0006@126.com>
2017-09-11 18:03:29 +0800

    //二分法查找

--- src/main/java/dataStructure/MyOrderArray.java
+++ src/main/java/dataStructure/MyOrderArray.java
@@ -38,7 +38,8 @@ public class MyOrderArray {
         if (size == 0) {    // 如果数组为空,直接插入第一个位置
             arr[size] = value;
         } else {
-            // 不为空,从最后一个开始,同数组里元素挨个比较,遇到大于value的值,则element向后移一位,直到element<value为止
+            // 不为空,从最后一个开始,同数组里元素挨个比较,遇到大于value的值,
+            // 则element向后移一位,直到element<value停止
             for (int i = size - 1; i >= 0; i--) {
                 if (arr[i] < value) {
                     arr[i + 1] = value;
@@ -103,24 +104,32 @@ public class MyOrderArray {
     }
 
     /**
-     * 在给定的范围内查找某个值
+     * 二分法在给定的有序数组内查找某个值:在有序数组中查找时,将数组根据中间值分为前后两部分,
+     * 用目标数组和数组的中间值比较,根据结果可以确定下一轮需要和哪部分进行比较,然后继续前面的
+     * 步骤。直到找到目标,或者确定数组中不存在目标值。
      * <p>
      * 判断中间位置的值是否和相等,如果相等则返回中间位置的下标;
      * 如果不相等,检查给定数组长度是否大于1,
      * 如果否,则说明未能找到value,返回-1;
      * 如果是,则根据中间值和value的大小关系判断下一次查找的范围,然后再次调用自身进行查找。
+     * <p>
+     * 使用二分法查找有什么好处?
+     * <p>
+     * 最差时间复杂度:O(log2N)  当输入量很大时,比较节约时间
+     * 假设共有N个元素,需要执行x次才能找到目标,最坏的情况是查找到最后一个元素时,
+     * 才能找到目标元素, 即 N / 2^x <= 1 => x<=log2N
      *
      * @param value 目标
      * @param start 起始下标
      * @param end   终止下标
-     * @return value在数组中的下标
+     * @return 目标在数组中的下标
      */
     private int search(long value, int start, int end) {
         long middle = arr[(start + end) / 2];
         if (middle == value) {
             return (start + end) / 2;
         } else {
-            if (start == end) {
+            if (start >= end) {
                 return -1;
             } else if (middle > value) {
                 return search(value, start, (start + end) / 2 - 1);
