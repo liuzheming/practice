cfb52df4cedc7f833dce2120d8276bd13875c04b
liuzheming <liu_0006@126.com>
2017-09-27 00:24:55 +0800

    //队列

---
+++ src/main/java/dataStructure/MyCycleQueue.java
@@ -0,0 +1,81 @@
+package dataStructure;
+
+import java.util.Arrays;
+
+/**
+ * Description:循环队列
+ * <p>
+ * Created by lzm on 2017/9/26.
+ */
+public class MyCycleQueue {
+
+    private Long[] arr;
+
+    private int front;
+
+    private int end;
+
+    private int eleNum;
+
+    public MyCycleQueue(int size) {
+        arr = new Long[size];
+        front = 0;
+        end = -1;
+        eleNum = 0;
+    }
+
+    public boolean isFull() {
+        return eleNum == arr.length;
+    }
+
+    public boolean isEmpty() {
+        return eleNum == 0;
+    }
+
+    @Override
+    public String toString() {
+        return "MyCycleQueue{" +
+                "arr=" + Arrays.toString(arr) +
+                ", front=" + front +
+                ", end=" + end +
+                ", eleNum=" + eleNum +
+                '}';
+    }
+
+    public void insert(long value) {
+        if (!isFull()) {
+            // 如果队列末尾下标到达数组边界且数组尚未全满,
+            // 则末尾转移至数组首位
+            if (end == arr.length - 1) {
+                end = -1;
+            }
+            arr[++end] = value;
+            eleNum++;
+        } else {
+            throw new IndexOutOfBoundsException("循环队列已满,插入失败[value=" + value + ";cycleQueue=" + this.toString() + "]");
+        }
+    }
+
+
+    public long remove() {
+        if (!isEmpty()) {
+            long value = arr[front++];
+            if (front > arr.length - 1) {
+                front = 0;
+            }
+            eleNum--;
+            return value;
+        } else {
+            throw new IndexOutOfBoundsException("循环队列为空,弹出失败[cycleQueue=" + this.toString() + "]");
+        }
+    }
+
+    public long peek() {
+        if (!isEmpty()) {
+            return arr[front];
+        } else {
+            throw new IndexOutOfBoundsException("循环队列为空,弹出失败[cycleQueue=" + this.toString() + "]");
+        }
+    }
+
+}
---
+++ src/main/java/dataStructure/MyQueue.java
@@ -0,0 +1,49 @@
+package dataStructure;
+
+/**
+ * Description:队列,先进先出
+ * <p>
+ * Created by lzm on 2017/9/26.
+ */
+public class MyQueue {
+
+
+    private long[] arr;
+
+    private int front;
+
+    private int end;
+
+    private int eleNum;
+
+    public MyQueue(int size) {
+        arr = new long[size];
+        front = 0;
+        end = -1;
+        eleNum = 0;
+    }
+
+
+    public void insert(int value) {
+        arr[++end] = value;
+        eleNum++;
+    }
+
+    public long remove() {
+        eleNum--;
+        return arr[front++];
+    }
+
+    public long peek() {
+        return arr[front];
+    }
+
+    public boolean isEmpty() {
+        return eleNum == 0;
+    }
+
+    public boolean isFull() {
+        return eleNum == arr.length;
+    }
+
+}
