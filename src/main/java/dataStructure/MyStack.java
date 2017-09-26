189b9055eb670da24d4acd0a434c3fb5076179fd
liuzheming <liu_0006@126.com>
2017-09-27 00:27:21 +0800

    //堆

---
+++ src/main/java/dataStructure/MyStack.java
@@ -0,0 +1,39 @@
+package dataStructure;
+
+/**
+ * Description:栈,先进后出
+ * <p>
+ * Created by lzm on 2017/9/26.
+ */
+public class MyStack {
+
+    private Long arr[];
+
+    private int top;
+
+    public MyStack(int size) {
+        arr = new Long[size];
+        top = -1;
+    }
+
+    public void push(long value) {
+        arr[++top] = value;
+    }
+
+    public long peek() {
+        return arr[top];
+    }
+
+    public long pop() {
+        return arr[top--];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == arr.length - 1;
+    }
+
+}
