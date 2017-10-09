4de5b6ea59b63ef34d558b79b50366619ba1eeea
liuzheming <liu_0006@126.com>
2017-10-09 14:52:45 +0800

    //链表

--- src/main/java/dataStructure/MyLinkedList.java
+++ src/main/java/dataStructure/MyLinkedList.java
@@ -1,84 +1,196 @@
 package dataStructure;
 
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
- * Description:
+ * Description: 在链表的两头,分别有一个节点作为逻辑上的开始节点,这样做的好处是,避免了很多特殊情况从而简化了编码
  * <p>
- * Created by lzm on 2017/9/26.
+ * Created by lzm on 2017/9/28.
  */
-public class MyLinkedList {
+public class MyLinkedList<T> implements Iterable<T> {
 
 
-    private int size;
+    private int theSize;
 
-    private Node first;
+    private int modCount = 0;
 
-    private Node last;
+    private Node<T> beginMarker;
 
+    private Node<T> endMarker;
 
     public MyLinkedList() {
-        size = 0;
-        first = null;
-        last = null;
-    }
-
-    public void add(Node node) {
-        if (isEmpty()) {
-            first = node;
-            last = node;
-        } else {
-            last.setNext(node);
-            last = node;
-        }
-        size++;
+        clear();
     }
 
 
-    public Node remove(int index) {
-        Node temp;
-        checkIndex(index);
-        if (index == 0) {
-            temp = first;
-            first = first.getNext();
-        } else {
-            temp = get(index - 1).getNext();
-            get(index - 1).setNext(temp.getNext());
-        }
-        size--;
-        return temp;
+    public void clear() {
+
+        beginMarker = new Node<>(null); // 表头部的标记节点:头结点
+        endMarker = new Node<>(null, beginMarker, null);   // 表尾部的标记节点:尾节点
+        beginMarker.next = endMarker;
+
+        theSize = 0;
+        modCount++;
     }
 
-    public Node get(int index) {
-        checkIndex(index);
-        Node temp = first; // 如果index==1,返回first
-        if (index > 0) {   // 如果index >1,逐个向后查找
-            for (int i = 1; i <= index; i++) {
-                temp = temp.getNext();
-            }
-        }
-        return temp;
-    }
-
-    public void checkIndex(int index) {
-        if (index > size - 1) {
-            throw new IndexOutOfBoundsException("下标越界 [index=" + index + "]");
-        }
+    public int size() {
+        return theSize;
     }
 
     public boolean isEmpty() {
-        return size == 0;
+        return theSize == 0;
     }
 
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        if (!isEmpty()) {
-            sb.append(first.getValue() + ",");
-            Node temp = first.getNext();
-            while (temp != null) {
-                sb.append(temp.getValue() + ",");
-                temp = temp.getNext();
+    public void add(T val) {
+        add(size(), val);
+    }
+
+    public void add(int idx, T val) {
+        addBefore(getNode(idx), val);
+    }
+
+
+    public T remove(int idx) {
+        return remove(getNode(idx));
+    }
+
+
+    public T set(int idx, T val) {
+        Node<T> node = getNode(idx);
+        T oldVal = node.data;
+        node.data = val;
+        return oldVal;
+    }
+
+    public T get(int idx) {
+        return getNode(idx).data;
+    }
+
+
+    private void addBefore(Node<T> current, T val) {
+        Node<T> newNode = new Node<>(val, current.prev, current);
+        current.prev = current.prev.next = newNode;
+        theSize++;
+        modCount++;
+    }
+
+    /**
+     * Removes the object contains in Node p.
+     *
+     * @param p the Node containing the object.
+     * @return the item was removed from the collection.
+     */
+    private T remove(Node<T> p) {
+        p.prev.next = p.next;
+        p.next.prev = p.prev;
+        theSize--;
+        modCount++;
+        return p.data;
+    }
+
+    /**
+     * Gets the Node at position idx,which must range from 0 to size().
+     *
+     * @param idx index of node being obtained.
+     * @return internal node corresponding to idx.
+     * @throws IndexOutOfBoundsException if idx is not between 0 and size().
+     */
+    private Node<T> getNode(int idx) {
+        if (idx < 0 || idx > size()) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        Node<T> current;
+        if (idx > size() >> 1) {
+            current = endMarker;
+            for (int i = size() - 1; i >= idx; i--) {
+                current = current.prev;
+            }
+        } else {
+            current = beginMarker;
+            for (int i = 0; i <= idx; i++) {
+                current = current.next;
             }
         }
-        return "[" + sb.substring(0, sb.length() - 1) + "]";
+        return current;
     }
 
+    @Override
+    public Iterator<T> iterator() {
+        return new MyLinkedListIterator();
+    }
+
+    private class MyLinkedListIterator implements Iterator<T> {
+
+        private Node<T> current = beginMarker.next;
+        private int expectedModCount = modCount;
+        private boolean okToRemove = false;
+
+        @Override
+        public boolean hasNext() {
+            return current != endMarker;
+        }
+
+        @Override
+        public T next() {
+
+            if (modCount != expectedModCount) {
+                throw new ConcurrentModificationException();
+            }
+
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            T nextVal = current.data;
+            current = current.next;
+            okToRemove = true;
+            return nextVal;
+        }
+
+        public void remove() {
+            if (modCount != expectedModCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (!okToRemove) {
+                throw new IllegalStateException();
+            }
+            MyLinkedList.this.remove(current.prev);
+            okToRemove = false;
+            expectedModCount++;
+        }
+
+    }
+
+    private static class Node<T> {
+
+        public Node(T data) {
+            this.data = data;
+        }
+
+        public Node(T data, Node<T> pre, Node<T> next) {
+            this.data = data;
+            this.prev = pre;
+            this.next = next;
+        }
+
+
+        private Node<T> prev;
+
+        private Node<T> next;
+
+        private T data;
+
+    }
+
+    @Override
+    public String toString() {
+        Iterator<T> it = this.iterator();
+        StringBuilder sb = new StringBuilder();
+        while (it.hasNext()) {
+            sb.append(it.next()).append(",");
+        }
+        return isEmpty() ? "[]" : "[" + sb.substring(0, sb.length() - 1) + "]";
+    }
 }
