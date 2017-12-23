ce7d2e879bb82d9638f39b44bc6cb0f5054a97a9
liuzheming <liu_0006@126.com>
2017-12-23 21:24:25 +0800

    //停止线程

---
+++ src/main/java/thread/section1/stopThread/StopThread1.java
@@ -0,0 +1,24 @@
+package thread.section1.stopThread;
+
+/**
+ * Description: 用循环来配合break来停止线程,但是只能停止执行循环内的代码
+ * <p>
+ * Created by lzm on 2017/12/22.
+ */
+public class StopThread1 extends Thread {
+
+    /**
+     * 测试使用标记来停止线程代码的执行,但是并不会停止线程
+     */
+    @Override
+    public void run() {
+        for (int i = 0; i < 500000; i++) {
+            System.out.println("i=" + i);
+            if (this.isInterrupted()) {
+                System.out.println("已标记为被打断,退出...");
+                break;
+            }
+        }
+        System.out.println("我被输出,说明已经退出for循环,但是线程并未停止!");
+    }
+}
---
+++ src/main/java/thread/section1/stopThread/StopThreadTest.java
@@ -0,0 +1,39 @@
+package thread.section1.stopThread;
+
+/**
+ * Description:
+ * <p>
+ * Created by lzm on 2017/12/22.
+ */
+public class StopThreadTest {
+
+
+    public static void main(String... args) {
+        test1();
+    }
+
+    private static void test() {
+
+        Thread thread = new StopThread3();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread.start();
+        thread.interrupt();
+
+    }
+
+    private static void test1() {
+        Thread thread = new StopThread5();
+        thread.start();
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+
+        }
+        thread.interrupt();
+    }
+
+}
