dd00acfe149ae3034e2c9a434c032a307951f286
liuzheming <liu_0006@126.com>
2018-01-04 13:04:01 +0800

    //监视器练习

---
+++ src/main/java/thread/watchDog/ServiceThread.java
@@ -0,0 +1,32 @@
+package thread.watchDog;
+
+/**
+ * Description:
+ * <p>
+ * Created by lzm on 2017/12/25.
+ */
+public class ServiceThread implements Runnable {
+
+
+    private String username;
+
+    private String password;
+
+    private Service service;
+
+    public ServiceThread() {
+
+    }
+
+    public ServiceThread(Service service, String username, String password) {
+        this.service = service;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    @Override
+    public void run() {
+        service.setUsernamePassword(username, password);
+    }
+}
