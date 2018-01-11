e209ab1106807a04769ee8ddaf358b32eac67a7a
liuzheming <liu_0006@126.com>
2018-01-11 10:55:31 +0800

    // 线程池测试

---
+++ src/main/java/thread/ThreadPoolCase/Task.java
@@ -0,0 +1,18 @@
+package thread.ThreadPoolCase;
+
+/**
+ * Description:
+ * <p>
+ * Created by lzm on 2018/1/11.
+ */
+public class Task implements Runnable {
+
+    @Override
+    public void run() {
+
+
+
+    }
+
+
+}
