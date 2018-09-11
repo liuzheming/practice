1dd9d8332e0ed47cdcd379416edeff8d4ea82508
liuzheming <liuzheming@jd.com>
2018-09-11 19:57:33 +0800

    // 无向图，邻接表实现

--- src/main/java/dataStructure/graph/Graph.java
+++ src/main/java/dataStructure/graph/Graph.java
@@ -1,4 +1,36 @@
 package dataStructure.graph;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Graph {
+
+    private int nodeCount;
+
+    private int edgeCount;
+
+    private Set<Integer>[] table;
+
+    public Graph(int nodeSize) {
+        nodeCount = nodeSize;
+        table = (Set<Integer>[]) new HashSet[nodeCount];
+        for (int i = 0; i < nodeSize; i++) {
+            table[i] = new HashSet<>();
+        }
+    }
+
+    public void addEdge(int start, int end) {
+        table[start].add(end);
+        table[end].add(start);
+        edgeCount++;
+    }
+
+    public int nodeCount() {
+        return nodeCount;
+    }
+
+    public int edgeCount() {
+        return edgeCount;
+    }
+
 }
