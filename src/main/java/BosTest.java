1e8d98c88d96aa5c288e105f9ca681ca9147e8d9
liuzheming01 <liuzheming01@baidu.com>
2020-11-24 13:26:12 +0800

    skiplist

--- src/main/java/BosTest.java
+++ src/main/java/BosTest.java
@@ -5,32 +5,6 @@ public class BosTest {
 
 
     public static void main(String... args) {
-//        BosConfiguration bosConfiguration = new BosConfiguration();
-//        bosConfiguration.setAccessKeyId("4ee7258072db484bb28db39b98dc3272");
-//        bosConfiguration.setSecretKey("43aad7d2938d4d2fa16d50b2eca98272");
-//        bosConfiguration.setEndpoint("https://bj.bcebos.com");
-//        bosConfiguration.setConnectionTimeoutInMillis(1000);
-//        bosConfiguration.setMaxConnections(1000);
-//        bosConfiguration.setSocketBufferSizeInBytes(1000);
-//        bosConfiguration.setConnectionTimeoutInMillis(1000);
-//        bosConfiguration.setSocketTimeoutInMillis(1000);
-//        BosClient bosClient = bosConfiguration.getBosClient(
-//                bosConfiguration.getBosClientConfiguration(
-//                        bosConfiguration.getDefaultBceCredentials()));
-//        BosObject object = bosClient.getObject("ipipe-agile",
-//                "/sa/online/com.baidu.agile.server.job.model.InnerJobParameterDetail/46109850");
-////                "sa/online/com.baidu.agile.server.job.model.innerJobParameterDetail/49387182");
-//        System.out.println(object.toString());
-//        InputStream is = object.getObjectContent();
-//
-//        GetObjectRequest getObjectRequest = new GetObjectRequest("ipipe-agile", "/sa/online/com.baidu.agile.server" +
-//                ".job.model.InnerJobParameterDetail/46109850");
-//        URL url = bosClient.generatePresignedUrl("ipipe-agile", "/sa/online/com.baidu.agile.server" +
-//                ".job.model" +
-//                ".InnerJobParameterDetail/46109850", 100000);
-//        System.out.println("sss:" + url.toString());
-//        ObjectMetadata objectMetadata = bosClient.getObject(getObjectRequest,
-//                new File("/Users/liuzheming01/Desktop", "filename"));
     }
 
 }
