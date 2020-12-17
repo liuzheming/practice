c01b312e11e2beb93e98e28a3e9e57f026f022b9
liuzheming <liu_0006@126.com>
2020-12-17 13:44:49 +0800

    Merge branch 'master' of https://github.com/liuzheming/practice
    # Conflicts:
    #	src/main/java/consistent/ClusterCenter.java
    #	src/main/java/consistent/ClusterCenterImpl.java
    #	src/main/java/consistent/ClusterNode.java

--- src/main/java/DiDiTest.java
+++ src/main/java/DiDiTest.java
@@ -1,9 +1,54 @@
-package PACKAGE_NAME;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Description:
  * <p>
  * Created by lzm on 2020/12/16.
  */
+@Slf4j
 public class DiDiTest {
+
+
+    public List<Integer> getArr(int[] arr) {
+
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+
+        Map<Integer, Integer> numberToCount = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (numberToCount.containsKey(arr[i])) {
+                int count = numberToCount.get(arr[i]);
+                count = count + 1;
+                numberToCount.put(arr[i], count);
+            } else {
+                numberToCount.put(arr[i], 1);
+            }
+        }
+
+        List<Integer> targetArr = new ArrayList<>();
+
+        for (Map.Entry<Integer, Integer> entry : numberToCount.entrySet()) {
+            if (entry.getValue() % 2 == 1) {
+                targetArr.add(entry.getKey());
+            }
+        }
+        return targetArr;
+    }
+
+
+    public static void main(String... args) {
+
+        int[] arr = new int[]{1, 2, 1, 2, 3, 4, 5, 6, 3, 5, 7};
+        DiDiTest diDiTest = new DiDiTest();
+        log.info("find reuslt:{}", diDiTest.getArr(arr));
+
+    }
+
+
 }
